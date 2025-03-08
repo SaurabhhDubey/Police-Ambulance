733382cf6c6ab291201645b7095017a1324c3d10
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-03-08 19:11:46 +0530

    update

---
+++ src/components/Portal/Portal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Portal () {
+
+    return (
+       <div
+       className="h-screen bg-cover bg-center bg-[url('https://drive.google.com/file/d/1YbyM-8waDchwAwoFN3KfgcwRiMIB1h_z/view?usp=drivesdk')]">
+      
+       
+        
+    <h1>sk</h1>
+       </div> 
+    );
+}

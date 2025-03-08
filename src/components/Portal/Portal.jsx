52f8d4f3ce78341b7c6935e9fd6f3cfaa44a28d3
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-03-08 20:03:32 +0530

    update

--- src/components/Portal/Portal.jsx
+++ src/components/Portal/Portal.jsx
@@ -4,11 +4,18 @@ export default function Portal () {
 
     return (
        <div
-       className="h-screen bg-cover bg-center bg-[url('https://drive.google.com/file/d/1YbyM-8waDchwAwoFN3KfgcwRiMIB1h_z/view?usp=drivesdk')]">
-      
-       
+       className="h-screen bg-cover opacity-90 flex justify-center bg-center bg-[url('https://res.cloudinary.com/dyjmgpb5p/image/upload/v1741441911/frontPortalimage_ulbasm.png')]">
+
+       <div className="flex space-x-12 p-36 " >
+        <div className="w-60 h-60 bg-blue-500/70 text-white p-4 flex justify-center items-center rounded-xl">
+        box 1
+        </div>
+        <div className="w-60 h-60 bg-green-500/70 text-white p-4 flex justify-center items-center rounded-xl">
+        box 2
+        </div>
+       </div>
+
+       </div>
         
-    <h1>sk</h1>
-       </div> 
     );
 }

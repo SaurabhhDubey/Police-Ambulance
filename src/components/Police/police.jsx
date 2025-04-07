de2ad79d126460dd4c2f4813cd83a70e1101de43
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-04-07 11:07:39 +0530

    text update

--- src/components/Police/police.jsx
+++ src/components/Police/police.jsx
@@ -6,6 +6,7 @@ const navigate = useNavigate()
   return (
     <div className="h-screen bg-cover opacity-90 flex justify-center items-center bg-center bg-[url('https://res.cloudinary.com/dyjmgpb5p/image/upload/v1742355389/portrait-male-security-guard-with-uniform_23-2150368732_qyyu5p.jpg')]">
    <div className='relative w-full h-full'>
+    <div className='text-white absolute top-40 left-10 text-lg'>Enter location to get Police station information</div>
     <div className='absolute top-10 left-10 right-10 flex items-center justify-between '>
       <div className='bg-black text-white px-4 py-2 rounded-lg  opacity-85 hover:cursor-pointer' onClick={()=>navigate("/")}>
         <span>MAIN Portal</span>
